26313c9d996213a21be7b947a8db8d59421a82a3
Abubakar Imoro <abubakarimoro27@gmail.com>
2024-10-15 16:39:07 +0000

    fixed some errors in the user models

--- models/user-models.js
+++ models/user-models.js
@@ -2,21 +2,22 @@ import {Schema, model} from "mongoose";
 const userSchema = new Schema({
     firstName:{
         type: String,
-        required: true
-    }
+    },
+
     lastName:{
-        type: String
-        required: true
-    }
+        type: String,
+    },
+
     email:{
         type: String,
         required: true,
         unique: true
-    }
+    },
+
     password:{
         type: String,
         required: true
     }
 })
 
-export const User = model('User', userSchema)
+export const User = model('User', userSchema);
